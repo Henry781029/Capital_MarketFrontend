8cb6e6e0d97afdaf2b6b3f142862a44f77a0aa03
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-10-22 02:01:03 -0500

    se ingresan componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Button, Box} from '@mui/material';
+import { PersonAdd } from '@mui/icons-material';
+import { makeStyles } from '@mui/styles';
+
+
+const useStyles = makeStyles((theme) =>({
+    root:{
+        color:"white",
+        bgColor: 'black',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        border: 0
+    }
+}))
 
 export const App = () => {
+
+    const classes = useStyles()
+
     return (
-        <div>
-            hola
+        <Box sx={{'& button': {m:12}}}>
+            <div>
+            <Button 
+            startIcon = {<PersonAdd/>}
+            className={classes.root}
+            >Ingrese Usuario</Button>
         </div>
+        </Box> 
     )
 }
 
